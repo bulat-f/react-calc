df7dd64d0512c03397adf2774d784ca05b2cf21b
Bulat F <b.fathrahmanov@cometrica.ru>
2020-10-09 14:53:41 +0300

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import "./App.css";
+
+const NUMBERS = [1, 2, 3, 4, 5, 7, 8, 9, 0];
+
+const OPERATIONS = {
+  add: (a, b) => a + b,
+};
 
 function App() {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const [result, setResult] = useState(0);
+  const [operation, setOperation] = useState("");
+
+  const handleClick = useCallback(
+    (event) => {
+      const digit = Number(event.target.getAttribute("data-number"));
+      if (operation) {
+        setResult(currentNumber);
+        setCurrentNumber(digit);
+      } else {
+        setCurrentNumber(currentNumber * 10 + digit);
+      }
+    },
+    [currentNumber, operation]
+  );
+  const handleOperationClick = useCallback(() => {
+    setOperation("add");
+  }, []);
+
+  const handleClickEql = useCallback(() => {
+    if (operation) {
+      setCurrentNumber(OPERATIONS[operation](currentNumber, result));
+      setOperation("");
+    }
+  }, [currentNumber, operation, result]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className="display">{currentNumber}</p>
+      <div className="buttonsContainer">
+        <div className="digitsContainer">
+          <div>
+            <button className="button topOperation">AC</button>
+            <button className="button topOperation">+/-</button>
+            <button className="button topOperation">%</button>
+          </div>
+          {NUMBERS.map((num) => (
+            <button
+              key={num}
+              data-number={num}
+              onClick={handleClick}
+              className="button digit"
+            >
+              {num}
+            </button>
+          ))}
+        </div>
+        <div className="rightOperationsContainer">
+          <button
+            className="button rightOperation"
+            onClick={handleOperationClick}
+          >
+            +
+          </button>
+          <button className="button rightOperation" onClick={handleClickEql}>
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
